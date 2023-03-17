e132fa1a5803b5652528883db63c984c234a1c63
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-17 19:34:03 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Button from '$lib/Button.svelte';
+	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
+	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte';
+</script>
+
+<Button
+	on:click|once={(event) => {
+		alert(true);
+	}}
+	let:isLeftHovered
+	size="small"
+	shadow
+>
+	<div style:width="20px" slot="leftContent">
+		{#if isLeftHovered}
+			<FaAngellist />
+		{:else}
+			<FaAllergies />
+		{/if}
+	</div>
+	Button Text
+</Button>
+
+<style>
+</style>
