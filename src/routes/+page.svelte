358dba6aa8203e3960058a209825cc59a5307e4d
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-20 16:29:46 +0530

    created mini routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
 <script>
-  import tippy from '../lib/actions/tippy';
+	import { onMount } from 'svelte';
+	import Home from '../lib/pages/Home.svelte';
+	import Settings from '../lib/pages/Settings.svelte';
+	let page;
 
-  let content = "Hello";
+	onMount(onRoutesChange);
+
+	function onRoutesChange() {
+		console.log(window.location.hash.slice(1));
+		if(window.location.hash){
+		const path = window.location.hash.slice(1);
+		if (path === '/') {
+			page = 'home';
+		} else if (path === '/settings') {
+			page = 'settings';
+		} else {
+			window.location.hash = '/';
+		}
+	}
+	}
 </script>
-<input bind:value={content}>
-<button use:tippy={{content:content, placement: 'right', theme:'light'}}>Button</button>
+
+<svelte:window on:hashchange={onRoutesChange} />
+
+<nav>
+	<a href="#/">Home</a>
+	<a href="#/settings">Setting</a>
+</nav>
+
+{#if page === 'home'}
+	<Home />
+{:else if page === 'settings'}
+	<Settings />
+{/if}
+
 <style>
 </style>
