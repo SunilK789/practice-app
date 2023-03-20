52ff1a11d351653448a7c1a3db1ab85b986bbe0e
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-20 12:30:46 +0530

    using actions

--- src/lib/transitions/fade.js
+++ src/lib/transitions/fade.js
@@ -1,6 +1,7 @@
 
 import {cubicInOut} from 'svelte/easing'
 
+// eslint-disable-next-line no-unused-vars
 export default function fade(node,{delay = 0, duration = 400, easing= cubicInOut, spin = 1}){
     const originalOpacity = +getComputedStyle(node).opacity;
     return {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,143 +1,15 @@
 <script>
-	import TodoList from '../lib/TodoList.svelte';
-	import { v4 as uuid } from 'uuid';
-	import { onMount, tick } from 'svelte';
-	import { debug } from 'svelte/internal';
-	import { fade, fly } from 'svelte/transition';
-	import spin from '../lib/transitions/spin';
-
-	let todoList;
-	let showHide = true;
-	let todos = null;
-	let error = null;
-	let isLoading = false;
-	let isAdding = false;
-	let disabledItems = [];
-
-	onMount(() => {
-		loadTodos();
-	});
-
-	async function loadTodos() {
-		isLoading = true;
-		await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10').then(async (response) => {
-			if (response.ok) {
-				todos = await response.json();
-			} else {
-				error = 'An error has occurred!';
-			}
-		});
-
-		isLoading = false;
-	}
-
-	let promise = loadTodos();
-
-	async function handleAddtodo(event) {
-		event.preventDefault();
-		isAdding = true;
-		await fetch('https://jsonplaceholder.typicode.com/todos', {
-			method: 'POST',
-			body: JSON.stringify({
-				title: event.detail.title,
-				completed: false
-			}),
-			headers: {
-				'Content-type': 'application/json;charset=UTF-8'
-			}
-		}).then(async (response) => {
-			if (response.ok) {
-				const todo = await response.json();
-				todos = [{ ...todo, id: uuid() },...todos];
-				todoList.clearInput();
-			} else {
-				alert('An error has occurred!');
-			}
-		});
-
-		isAdding = false;
-
-		await tick();
-		todoList.focusInput();
-	}
-	async function handlRemoveTodos(event) {
-		const id = event.detail.id;
-		if (disabledItems.includes(id)) return;
-		disabledItems = [...disabledItems, id];
-
-		await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-			method: 'DELETE'
-		}).then(async (response) => {
-			if (response.ok) {
-				todos = todos.filter((t) => t.id !== event.detail.id);
-				todoList.clearInput();
-			} else {
-				alert('An error has occurred!');
-			}
-		});
-		disabledItems = disabledItems.filter((itemId) => itemId !== id);
-	}
-	function toggleCheckBox(event) {
-		const value = event.detail.value;
-		const id = event.detail.id;
-
-		if (disabledItems.includes(id)) return;
-		disabledItems = [...disabledItems, id];
-
-		fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-			method: 'PATCH',
-			body: JSON.stringify({
-				completed: value
-			}),
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			}
-		}).then(async (response) => {
-			if (response.ok) {
-				const updatedTodo = await response.json();
-				console.log(updatedTodo);
-				todos = todos.map((todo) => {
-					if (todo.id === id) {
-						return updatedTodo;
-					}
-
-					return { ...todo };
-				});
-			} else {
-				alert('An error has occurred!');
-			}
-		});
-
-		disabledItems = disabledItems.filter((itemId) => itemId !== id);
-	}
+	import longpress from '../lib/actions/longpress';
+	let showButton = true;
+	let duration = 1000;
 </script>
 
 <label>
-	<input type="checkbox" bind:checked={showHide} />
-	Show/Hide List
+	<input type="checkbox" bind:checked={showButton} /> Toggle
 </label>
-
-{#if showHide}
-	<div transition:spin style:max-width="800px" style="opacity:0.5">
-		<TodoList
-			bind:this={todoList}
-			{todos}
-			{error}
-			{isLoading}
-			{disabledItems}
-			disabelAdding={isAdding}
-			scrollOnAdd="bottom"
-			on:addtodo={handleAddtodo}
-			on:removetodo={handlRemoveTodos}
-			on:toggleCheckBox={toggleCheckBox}
-		/>
-	</div>
-	{#if todos}
-		<p>
-			Total Todos:{#key todos.length}
-				<span style:display="inline-block" in:fly|local={{ y: -10 }}> {todos.length}</span>{/key}
-		</p>
-	{/if}
+<input type="range" bind:value={duration} max="10000" step="100" />
+{#if showButton}
+	<button use:longpress={{ duration: duration }}>Hello</button>
 {/if}
 
 <style>
