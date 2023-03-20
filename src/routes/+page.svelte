686d1c67b123551f53023ac3266b3890fdadfa92
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-20 15:59:21 +0530

    tippy button tooltip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script>
-	import longpress from '../lib/actions/longpress';
-	let showButton = true;
-	let duration = 1000;
+  import { onMount } from 'svelte';
+	import tippy  from 'tippy.js';
+	import 'tippy.js/dist/tippy.css';
+
+	let button;
+	onMount(()=>{
+		tippy('.tooltip',{
+			content:"this is a tooltip"
+		})
+		tippy(button,{
+			content:"another this is a tooltip"
+		})
+	})
 </script>
 
-<label>
-	<input type="checkbox" bind:checked={showButton} /> Toggle
-</label>
-<input type="range" bind:value={duration} max="10000" step="100" />
-{#if showButton}
-	<button use:longpress={{ duration: duration }} on:longpress={()=>{alert("pressed")}}>Hello</button>
-{/if}
-
+<button class="tooltip">Button</button>
+<button bind:this={button}>Button</button>
+<button class="tooltip" data-tippy-content="Hello">Button</button>
 <style>
 </style>
