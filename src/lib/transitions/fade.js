0790b50af65e4970a87dfa47124c7467794207d2
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-20 11:15:50 +0530

    custom transition for fade

--- src/lib/transitions/fade.js
+++ src/lib/transitions/fade.js
@@ -1,16 +1,15 @@
 
 import {cubicInOut} from 'svelte/easing'
 
-export default function spin(node,{delay = 0, duration = 400, easing= cubicInOut, spin = 1}){
+export default function fade(node,{delay = 0, duration = 400, easing= cubicInOut, spin = 1}){
     const originalOpacity = +getComputedStyle(node).opacity;
     return {
         delay,
         duration,
         easing,
-        css:(t)=>{
+        tick:(t)=>{
             return `
             opacity: ${t * originalOpacity};
-            transform:scale(${t}) rotate(${spin * 360 *t}deg)
             `;
         }
     }
