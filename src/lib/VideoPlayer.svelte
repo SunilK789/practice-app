04325dc76040fc98c4b315c232e68e809fb029cd
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-21 21:26:40 +0530

    created video component

---
+++ src/lib/VideoPlayer.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let src = undefined;
+	export let width;
+	export let height;
+
+	let paused = true;
+	let currentTime = 0;
+	let volume = 0;
+</script>
+
+<video class:playing={!paused} {src} {width} {height} controls muted bind:paused bind:volume bind:currentTime /><br />
+<button on:click={()=>{
+    paused = !paused;
+}}>{paused ? 'Play' : 'Pause'} </button>
+<p>{currentTime}</p>
+<p>Volume: {volume}</p>
+
+
+<style>
+    video.playing{
+        outline: 4px solid #ff3e00;
+    }
+    </style>
--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,26 +1,12 @@
 <script>
-	import { Stage, Layer, Rect } from '../konva';
+    import VideoPlayer from '../VideoPlayer.svelte';
+    import videoLink from '$lib/TestVideo.mp4'
 
-	let container;
-	let showConvas = true;
+    const videoPath = "$lib/TestVideo.mp4"
 </script>
 
-<div bind:this={container} />
-
-<label>
-	<input type="checkbox" bind:checked={showConvas} />
-	Show/Hide convas</label
->
-{#if showConvas}
-	<Stage width={300} height={400}>
-		<Layer draggable>
-			<Rect x={20} y={20} fill="blue" width={150} height={100} />
-		</Layer>
-        <Layer>
-			<Rect x={50} y={50} fill="aqua" width={100} height={100} />
-		</Layer>
-         <Layer>
-			<Rect x={70} y={70} fill="green" width={50} height={150} />
-		</Layer>
-	</Stage>
-{/if}
+<div class="flex">
+<VideoPlayer height={150} width={200} src={videoLink}></VideoPlayer>
+<VideoPlayer height={150} width={200} src={videoLink}></VideoPlayer>
+<VideoPlayer height={150} width={200} src={videoLink}></VideoPlayer>
+</div>
